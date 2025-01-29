daf34a1ad85ff05df283569e70c3369414d47eee
Stuart George <stuart@accentdesign.co.uk>
2025-01-29 23:20:20 +0000

    simplify the examples

--- examples/form/main.go
+++ examples/form/main.go
@@ -9,55 +9,62 @@ import (
 	"time"
 )
 
-func field(controls ...templ.Component) *gtml.Element {
-	return gtml.Div(gtml.Attrs{"class": "space-y-2"}, controls...)
+type option struct {
+	value, label string
 }
 
 func input(id, label, placeholder, typ, help string, required bool) *gtml.Element {
-	return field(
-		gtml.Label(gtml.Attrs{"class": "text-sm font-medium leading-none", "for": id}, gtml.Text(label)),
-		gtml.Input(gtml.Attrs{"class": "flex w-full rounded-md border px-3 py-2 text-sm", "id": id, "placeholder": placeholder, "required": required, "type": typ}),
-		gtml.P(gtml.Attrs{"class": "text-sm text-gray-500"}, gtml.Text(help)),
+	return gtml.Div(
+		gtml.Attrs{"class": "field"},
+		gtml.Label(gtml.Attrs{"for": id}, gtml.Text(label)),
+		gtml.Input(gtml.Attrs{"class": "input", "id": id, "placeholder": placeholder, "required": required, "type": typ}),
+		gtml.If(help != "", gtml.P(gtml.Attrs{"class": "help"}, gtml.Text(help))),
 	)
 }
 
 func textarea(id, label, placeholder, help string, required bool) *gtml.Element {
-	return field(
-		gtml.Label(gtml.Attrs{"class": "text-sm font-medium leading-none", "for": id}, gtml.Text(label)),
-		gtml.Textarea(gtml.Attrs{"class": "flex w-full rounded-md border px-3 py-2 text-sm", "id": id, "placeholder": placeholder, "required": required}),
-		gtml.P(gtml.Attrs{"class": "text-sm text-gray-500"}, gtml.Text(help)),
+	return gtml.Div(
+		gtml.Attrs{"class": "field"},
+		gtml.Label(gtml.Attrs{"for": id}, gtml.Text(label)),
+		gtml.Textarea(gtml.Attrs{"class": "input", "id": id, "placeholder": placeholder, "required": required}),
+		gtml.If(help != "", gtml.P(gtml.Attrs{"class": "help"}, gtml.Text(help))),
 	)
 }
 
-func dropdown(id, label, help string, required bool, options []templ.Component) *gtml.Element {
-	return field(
-		gtml.Label(gtml.Attrs{"class": "text-sm font-medium leading-none", "for": id}, gtml.Text(label)),
-		gtml.Select(gtml.Attrs{"class": "flex w-full rounded-md border px-3 py-2 text-sm", "id": id, "required": required}, options...),
-		gtml.P(gtml.Attrs{"class": "text-sm text-gray-500"}, gtml.Text(help)),
+func dropdown(id, label, help string, required bool, options []option) *gtml.Element {
+	return gtml.Div(
+		gtml.Attrs{"class": "field"},
+		gtml.Label(gtml.Attrs{"for": id}, gtml.Text(label)),
+		gtml.Select(
+			gtml.Attrs{"class": "input", "id": id, "required": required},
+			gtml.Map[option](options, func(o option) templ.Component {
+				return gtml.Option(gtml.Attrs{"value": o.value}, gtml.Text(o.label))
+			})...,
+		),
+		gtml.If(help != "", gtml.P(gtml.Attrs{"class": "help"}, gtml.Text(help))),
 	)
 }
 
-var sexOptions = []templ.Component{
-	gtml.Option(gtml.Attrs{"value": ""}, gtml.Text("Please Select...")),
-	gtml.Option(gtml.Attrs{"value": "male"}, gtml.Text("Male")),
-	gtml.Option(gtml.Attrs{"value": "female"}, gtml.Text("Female")),
-	gtml.Option(gtml.Attrs{"value": "other"}, gtml.Text("Other")),
+var genderOptions = []option{
+	{value: "", label: "Please Select..."},
+	{value: "male", label: "Male"},
+	{value: "female", label: "Female"},
 }
 
 var form = gtml.Form(
-	gtml.Attrs{"class": "grid gap-10"},
+	gtml.Attrs{"class": "form"},
 	gtml.Fieldset(
-		gtml.Attrs{"class": "grid gap-10"},
-		gtml.Legend(gtml.Attrs{"class": "mb-10"}, gtml.Text("Your Details")),
-		input("name", "Name", "eg: Sam Smith", "text", "Enter your full name.", true),
-		input("email", "Email", "eg: sam@example.com", "email", "Enter your email address.", true),
-		dropdown("sex", "Sex", "This is your sex.", false, sexOptions),
+		gtml.NA,
+		gtml.Legend(gtml.NA, gtml.Text("Your Details")),
+		input("name", "Name", "eg: Sam Smith", "text", "Enter your name.", true),
+		input("email", "Email", "eg: sam@example.com", "email", "Enter your email.", true),
+		dropdown("gender", "Gender", "Enter your gender.", false, genderOptions),
 		textarea("comments", "Comments", "eg: Please sent me your number.", "What do you want?", false),
 	),
 	gtml.Div(
-		gtml.Attrs{"class": "flex items-center justify-between"},
-		gtml.Button(gtml.Attrs{"class": "rounded-md px-4 py-2 text-sm font-medium bg-black text-white", "type": "submit"}, gtml.Text("Submit")),
-		gtml.A(gtml.Attrs{"class": "text-blue-600 hover:underline", "href": "#"}, gtml.Text("Some random link")),
+		gtml.Attrs{"class": "footer"},
+		gtml.Button(gtml.Attrs{"class": "button", "type": "submit"}, gtml.Text("Submit")),
+		gtml.A(gtml.Attrs{"href": "#"}, gtml.Text("Some random link")),
 	),
 )
 
--- examples/paginate/main.go
+++ examples/paginate/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/a-h/templ"
 	"github.com/accentdesign/gtml"
 	"io"
 	"os"
@@ -26,33 +27,31 @@ func (t *Table) Header() *gtml.Element {
 }
 
 func (t *Table) Body() *gtml.Element {
-	body := gtml.TBody(gtml.NA)
 	start := (t.CurrentPage-1)*t.Limit + 1
 	end := t.CurrentPage * t.Limit
-	for i := start; i <= end; i++ {
-		body.AddChildren(gtml.TR(
-			gtml.NA,
-			gtml.TD(gtml.NA, gtml.Text(fmt.Sprintf("%d", i))),
-		))
-	}
-	return body
+	return gtml.TBody(
+		gtml.NA,
+		gtml.Range(start, end, func(i int) templ.Component {
+			return gtml.TR(
+				gtml.NA,
+				gtml.TD(gtml.NA, gtml.Text(fmt.Sprintf("%d", i))),
+			)
+		})...,
+	)
 }
 
 func (t *Table) Footer() *gtml.Element {
-	td := gtml.TD(gtml.NA)
-	if t.CurrentPage > 1 {
-		td.AddChildren(gtml.A(
+	return gtml.TD(
+		gtml.NA,
+		gtml.If(t.CurrentPage > 1, gtml.A(
 			gtml.NA,
 			gtml.Text(fmt.Sprintf("Page %d", t.CurrentPage-1)),
-		))
-	}
-	if t.CurrentPage < t.TotalPages {
-		td.AddChildren(gtml.A(
+		)),
+		gtml.If(t.CurrentPage < t.TotalPages, gtml.A(
 			gtml.NA,
 			gtml.Text(fmt.Sprintf("Page %d", t.CurrentPage+1)),
-		))
-	}
-	return td
+		)),
+	)
 }
 
 func (t *Table) Render(ctx context.Context, w io.Writer) error {
